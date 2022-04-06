af6147c60a7c349701c0bfae79436615bfdcba79
deshpandeprajakta <deshpande.pa02@gmail.com>
2022-04-06 18:01:44 +0530

    First Test case Program

---
+++ src/main/java/org/matrix/sigma/prajakta/selenium/TestFirstScript.java
@@ -0,0 +1,41 @@
+package org.matrix.sigma.prajakta.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestFirstScript {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//a[text()='Log in']")).click();
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("Email")).sendKeys("prajakta@gmail.com");
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("Password")).sendKeys("prajakta123");
+
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[text()='Log in']")).click();
+
+        WebElement errorMessage=driver.findElement(By.xpath("\"//div[text()='Login was unsuccessful. Please correct the errors and try again.']\""));
+        boolean status=errorMessage.isDisplayed();
+
+        if(status){
+
+            System.out.println("Test case is pass");
+        } else {
+            System.out.println("Test case is fail");
+        }
+
+        Thread.sleep(4000);
+        driver.close();
+
+    }
+}
