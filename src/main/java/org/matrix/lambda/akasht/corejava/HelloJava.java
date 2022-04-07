720c72ddd74482376525a03cb4bba667ab3d30b4
akash <Akash@7500>
2022-04-07 13:06:44 +0530

    Local Variables

--- src/main/java/org/matrix/lambda/akasht/corejava/HelloJava.java
+++
@@ -1,8 +0,0 @@
-package org.matrix.lambda.akasht.corejava;
-
-public class HelloJava {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, Akash Cool budy");
-    }
-}
