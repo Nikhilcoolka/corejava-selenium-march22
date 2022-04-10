d02dc4617d2c7460706e8acf4ab3b0d1b9618642
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 13:16:44 +0530

    MAT-13-NishaV: Declaration of variables

---
+++ src/main/java/org/matrix/lambda/nisha/methods/Student.java
@@ -0,0 +1,19 @@
+package org.matrix.lambda.nisha.methods;
+
+//Let's understand the problem if we don't use this keyword by the example given below:
+
+public class Student {
+
+        int rollno;
+        String name;
+        float fee;
+        Student(int rollno,String name,float fee){
+            rollno=rollno;
+            name=name;
+            fee=fee;
+        }
+
+
+    }
+
+
