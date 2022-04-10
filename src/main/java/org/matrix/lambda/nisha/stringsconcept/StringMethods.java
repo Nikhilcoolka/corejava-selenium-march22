3d17d4b280f3496567cc9e575ad2d718f5d868e7
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 12:23:48 +0530

    MAT-13-NishaV: length

--- src/main/java/org/matrix/lambda/nisha/stringsconcept/StringMethods.java
+++ src/main/java/org/matrix/lambda/nisha/stringsconcept/StringMethods.java
@@ -6,5 +6,9 @@ public class StringMethods {
     //Java counts positions from zero.
     //0 is the first position in a string, 1 is the second, 2 is the third ...
 
+    public static void main(String[] args) {
 
+        String object = "Mobile";
+        System.out.println("Length of String = " + object.length());
+        System.out.println(args.length);
 }
