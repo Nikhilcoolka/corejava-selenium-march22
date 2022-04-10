e11a1f688a955bee05d3a86cacf52a1cb93bc3bf
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 13:36:28 +0530

    MAT-13-NishaV: Local variables

---
+++ src/main/java/org/matrix/lambda/nisha/methods/TestMethodsConceptNew.java
@@ -0,0 +1,10 @@
+package org.matrix.lambda.nisha.methods;
+
+//      concept
+//      Local variables has the highest priority than Instance variables
+//      if we have same variable names
+
+public class TestMethodsConceptNew {
+
+
+}
