cbd6a718f804cbf73b694621c8d63ac6c0697097
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 13:34:43 +0530

    MAT-13-NishaV: Methods without parameters

--- src/main/java/org/matrix/lambda/nisha/methods/TestMethodsConcept.java
+++ src/main/java/org/matrix/lambda/nisha/methods/TestMethodsConcept.java
@@ -5,6 +5,17 @@ package org.matrix.lambda.nisha.methods;
 // eg. = public void a(){}
 //     = public static void b()
 
+public class TestMethodsConcept {
 
+    // --Instance Method---
+    public void a() {                                           // method declaration
+        System.out.println("Im in instance a method ");         // method implementation
+    }
+
+    // --Static Method---
+    public static void b() {
+        System.out.println("Im in static b method ");
+    }
 
 }
+
