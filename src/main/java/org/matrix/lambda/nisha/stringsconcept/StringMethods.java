90930fd6e8e1f786b86314235efbe344740a489d
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 12:22:29 +0530

    MAT-13-NishaV: String Methods

---
+++ src/main/java/org/matrix/lambda/nisha/stringsconcept/StringMethods.java
@@ -0,0 +1,10 @@
+package org.matrix.lambda.nisha.stringsconcept;
+
+
+public class StringMethods {
+
+    //Java counts positions from zero.
+    //0 is the first position in a string, 1 is the second, 2 is the third ...
+
+
+}
