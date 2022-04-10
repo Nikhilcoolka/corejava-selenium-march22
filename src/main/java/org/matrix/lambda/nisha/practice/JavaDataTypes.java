114daba48fecc92ce1f9fcea1056ebcc9f3b5502
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 12:07:53 +0530

    MAT-13-NishaV: Data types -byte

---
+++ src/main/java/org/matrix/lambda/nisha/practice/JavaDataTypes.java
@@ -0,0 +1,15 @@
+package org.matrix.lambda.nisha.practice;
+
+public class JavaDataTypes {
+
+    public static void main(String[] args) {
+
+        byte num1 = 100;
+        System.out.println(num1);
+        System.out.println("byte size = 1 byte");
+        System.out.println("Stores whole numbers from -128 to 127");
+        System.out.println("--------------------------------------------------");
+
+
+    }
+}
