d9ad220c93afe2986515ad16ad42ad8f6dadb742
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 12:49:34 +0530

    MAT-13-NishaV: String Object Creation

---
+++ src/main/java/org/matrix/lambda/nisha/stringsconcept/TestString1.java
@@ -0,0 +1,16 @@
+package org.matrix.lambda.nisha.stringsconcept;
+
+import java.lang.String;
+import java.lang.System;
+
+public class TestString1 {
+
+    public static void main(String[] args) {
+
+        // Returns different HashCode for same filename
+        String str1 = new String("Vijayalaxmi");     // Hashcode = 1118140819
+        String str2 = new String("Vijayalaxmi");     // Hashcode = 1975012498
+
+
+    }
+}
