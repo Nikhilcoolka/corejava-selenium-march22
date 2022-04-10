08dad2b02a266c84c2a5dac0d924ade17013e0b5
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 13:32:08 +0530

    MAT-13-NishaV: Types of Methods

---
+++ src/main/java/org/matrix/lambda/nisha/methods/TestMethodsConcept.java
@@ -0,0 +1,10 @@
+package org.matrix.lambda.nisha.methods;
+
+//-------Types of methods---------
+// 1. Methods Without Parameters
+// eg. = public void a(){}
+//     = public static void b()
+
+
+
+}
