1f1ad8785b9204c757a60c8c4878ed78a30a894a
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 12:32:09 +0530

    MAT-13-NishaV: String literal

---
+++ src/main/java/org/matrix/lambda/nisha/stringsconcept/TestString.java
@@ -0,0 +1,17 @@
+package org.matrix.lambda.nisha.stringsconcept;
+
+import java.lang.String;
+
+public class TestString {
+
+    public static void main(String[] args) {
+
+        String str1 = "Nisha";                          // 1.String Literal(SCP = String Constant Pool)
+        String str2 = new String("Nisha");      //  2.Object Creation
+        System.out.println(str1);
+
+
+    }
+}
+
+//String is a final class that means we cannot inherit the class.
