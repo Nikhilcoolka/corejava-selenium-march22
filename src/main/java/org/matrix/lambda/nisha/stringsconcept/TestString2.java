66cfcac2d82cd44977d04daec58594a26c412ac7
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 13:06:45 +0530

    MAT-13-NishaV: Different Hashcodes

---
+++ src/main/java/org/matrix/lambda/nisha/stringsconcept/TestString2.java
@@ -0,0 +1,15 @@
+package org.matrix.lambda.nisha.stringsconcept;
+
+
+public class TestString2 {
+
+    public static void main(String[] args) {
+
+        String str1 = new String("Vijaylaxmi");     // Hashcode = 1118140819
+        String str2 = new String("Vijaylaxmi");     // Hashcode = 1975012498
+        System.out.println("Address of str1: " + System.identityHashCode(str1));
+        System.out.println("Address of str2: " + System.identityHashCode(str2));
+
+    }
+
+}
