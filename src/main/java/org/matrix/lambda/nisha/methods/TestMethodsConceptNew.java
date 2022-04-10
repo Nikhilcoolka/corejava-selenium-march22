eb6a4b9f6b1dfa92fc846a7d532fea2431b9cbc8
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 13:38:16 +0530

    MAT-13-NishaV: Instance variables

--- src/main/java/org/matrix/lambda/nisha/methods/TestMethodsConceptNew.java
+++ src/main/java/org/matrix/lambda/nisha/methods/TestMethodsConceptNew.java
@@ -10,5 +10,13 @@ public class TestMethodsConceptNew {
     int a = 23;
     int b = 34;
 
+    // Local Variables
+    public void addition(int a, int b) {
+        System.out.println(a+b);
+        System.out.println(this.a+this.b);
+    }
+    // "this" can be used to refer current class instance variable.
+
+
 
 }
