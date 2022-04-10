580873123315890933d621f11bd2f79782820b6e
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-10 12:18:48 +0530

    MAT-13-NishaV: Inheritance - Parent

---
+++ src/main/java/org/matrix/lambda/nisha/inheritance/Parent.java
@@ -0,0 +1,13 @@
+package org.matrix.lambda.nisha.inheritance;
+
+public class Parent extends GrandParent{
+
+    int d = 4;
+    int i = 786;
+
+    public void m2() {
+        System.out.println("Parent :: m2 Method");
+    }
+
+}
+
