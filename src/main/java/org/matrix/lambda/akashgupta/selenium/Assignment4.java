f2c65cc23631a5592f245f93ec3a269f18fc0e11
AkashNGupta <akashngupta2116@gmail.com>
2022-04-13 16:28:22 +0530

    Created final class for system property

--- src/main/java/org/matrix/lambda/akashgupta/selenium/Assignment4.java
+++ src/main/java/org/matrix/lambda/akashgupta/selenium/Assignment4.java
@@ -2,13 +2,12 @@ package org.matrix.lambda.akashgupta.selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+
+import static org.matrix.lambda.akashgupta.selenium.setDriver.driver;
 
 public class Assignment4 {
     public static void main(String[] args) {
-        System.setProperty("webdriver.edge.driver", "C:\\Users\\akash\\Downloads\\edgedriver_win64\\msedgedriver.exe");
-        WebDriver driver = new EdgeDriver();
+        setDriver.setDriverProperty("chrome");
         driver.get("https://www.facebook.com/login.php");
         Dimension size = new Dimension(800, 800);
         driver.manage().window().setSize(size);
@@ -17,7 +16,7 @@ public class Assignment4 {
         driver.findElement(By.tagName("button")).click();
         Boolean flag = driver.findElement(By.xpath("//div[text()='Wrong credentials']")).isDisplayed();
         if (flag) {
-            System.out.println("You have entered Wrong credentials:\n" + "KInvalid username or password");
+            System.out.println("You have entered Wrong credentials:\n" + "Invalid username or password");
         }
         driver.close();
     }
