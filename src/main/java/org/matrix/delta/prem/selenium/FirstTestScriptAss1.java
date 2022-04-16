ca64a821e2f12ed91f1568d20f1bfc7333a686b0
Premp <prem74729@gmail.com>
2022-04-16 18:26:19 +0530

    First Selenium Script

--- src/main/java/org/matrix/delta/prem/selenium/FirstTestScriptAss1.java
+++ src/main/java/org/matrix/delta/prem/selenium/FirstTestScriptAss1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FirstTestScript {
+public class FirstTestScriptAss1 {
 
     public static void main(String[] args) {
 
