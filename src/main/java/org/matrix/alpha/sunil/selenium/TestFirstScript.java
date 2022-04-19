e767bf8ec72f02a5184ddd0fc60cd7fcacc6e708
akash <Akash@7500>
2022-04-19 18:07:34 +0530

    Selenium Practise

---
+++ src/main/java/org/matrix/alpha/sunil/selenium/TestFirstScript.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestFirstScript {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://demo.nopcommerce.com");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//a[text ()='Log in']")).click();
+        driver.findElement(By.id("Email")).sendKeys("sunil@gmail.com");
+        driver.findElement(By.id("Password")).sendKeys("Sunil123");
+        driver.findElement(By.xpath("//button [text ()='Log in']")).click();
+
+        WebElement errorMessage = driver.findElement(By.xpath("//div[text()='Login was unsuccessful. Please correct the errors and try again.']"));
+        boolean status = errorMessage.isDisplayed();
+        if (status) {
+            System.out.println("Test case pass");
+        } else {
+            System.out.println("Test case fail");
+        }
+        driver.close();
+    }
+}
