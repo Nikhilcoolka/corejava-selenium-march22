a4eb80bdc6fd4df62b62e25bb6dea44812e72c02
jagukudale <jagannath.kudale@gmail.com>
2022-04-19 12:40:37 +0530

    MAT-37 Jagannath Array Concept

---
+++ src/main/java/org/matrix/omega/jagannath/corejava/arrayconcept/TestArrayConcept.java
@@ -0,0 +1,14 @@
+package org.matrix.omega.jagannath.corejava.arrayconcept;
+
+public class TestArrayConcept {
+    public static void main(String[] args) {
+        int []arr=new int[3];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        for(int i=0;i<3;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
