c1d1f63541a749e2f703017217623cd1af4432a8
Kamlesh Dandi <dandi8626@gmail.com>
2022-04-20 19:03:34 +0530

    Assignment 5

--- src/main/java/org/matrix/alpha/kamlesh/selenium/assignments/Assignment4.java
+++ src/main/java/org/matrix/alpha/kamlesh/selenium/assignments/Assignment4.java
@@ -18,16 +18,16 @@ public class Assignment4 {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.facebook.com/login/");
-        //  Thread.sleep(1500);
+        Thread.sleep(1000);
         driver.manage().window().maximize();
         driver.findElement(By.xpath("//input[@placeholder='Email address or phone number']")).click();
-        //Thread.sleep(1500);
+        Thread.sleep(1000);
         driver.findElement(By.id("email")).sendKeys("kamal124@gmail.com");
-        //Thread.sleep(1500);
+        Thread.sleep(1000);
         driver.findElement(By.id("pass")).sendKeys("12345678");
-        //Thread.sleep(1500);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//button[@name='login']")).click();
-       // Thread.sleep(1500);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//a [@class='_39g9']")).click();
         WebElement errorMessage=driver.findElement(By.xpath("//div[@class='_9ay7']"));
         boolean status=errorMessage.isDisplayed();
