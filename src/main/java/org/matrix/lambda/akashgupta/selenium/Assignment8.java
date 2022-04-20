17ea43fa2d4e08b7f34a567e0ed1a4d6a2833c16
AkashNGupta <akashngupta2116@gmail.com>
2022-04-20 12:44:00 +0530

    Assignment8 : added resetPassword method if login failed

--- src/main/java/org/matrix/lambda/akashgupta/selenium/Assignment8.java
+++ src/main/java/org/matrix/lambda/akashgupta/selenium/Assignment8.java
@@ -2,6 +2,7 @@ package org.matrix.lambda.akashgupta.selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,10 +24,31 @@ public class Assignment8 {
 
     }
 
+    void resetPassword() throws InterruptedException {
+        System.out.println("Trying to reset password  ");
+        driver.findElement(By.xpath("//a[text()='Forgot your password?']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("securityAuthentication_userName")).sendKeys("Admin");
+        driver.findElement(By.id("btnSearchValues")).click();
+        Thread.sleep(5000);
+        System.out.println("Please contact HR management in order to reset the password");
+
+    }
+
     @Test
     void ohrmTabMenu() throws IOException, InterruptedException {
         WebDriverManager.chromedriver().setup();
         login();
+        try {
+            WebElement loginFailed = driver.findElement(By.id("spanMessage"));
+            if (loginFailed.isDisplayed()) {
+                System.out.println("login failed: " + loginFailed.getText());
+                resetPassword();
+                driver.close();
+                return;
+            }
+        } catch (NoSuchElementException e) {
+        }
         List<WebElement> tabs = driver.findElements(By.xpath("//ul[@id='mainMenuFirstLevelUnorderedList']/li"));
         if (tabs.size() != 11) {
             System.out.println("Tabs missing");
