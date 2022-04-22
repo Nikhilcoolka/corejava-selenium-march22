deeca8e2e4c9058d37f54d4f8667d8ba288dcf8f
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-23 00:11:37 +0530

    MAT-13-NishaV: HTML

---
+++ src/main/java/org/matrix/lambda/nisha/selenium/TestFirstScript.java
@@ -0,0 +1,45 @@
+package org.matrix.lambda.nisha.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+
+public class TestFirstScript {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","D:\\Ni\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();           // Driver Drives the chrome page for Intellij
+        driver.get("https://demo.nopcommerce.com/");        // Chrome website
+
+        driver.manage().window().maximize();                // Maximize the window
+
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[text()='Log in']")).click();           // Click on login
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("Email")).sendKeys("nishavsingam@gmail.com");          // Enter your email
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("Password")).sendKeys("Nisha@123");                    // Enter password
+
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[text()='Log in']")).click();                           // Click on login
+
+        WebElement errorMessage = driver.findElement(By.xpath("//div[text()='Login was unsuccessful. Please correct the errors and try again.'"));
+         boolean status = errorMessage.isDisplayed();
+
+         if (status)
+         {
+             System.out.println("Test case passed.");
+         }else
+         {
+             System.out.println("Test case failed.");
+         }
+
+         Thread.sleep(2000);
+         driver.close();
+    }
+}
