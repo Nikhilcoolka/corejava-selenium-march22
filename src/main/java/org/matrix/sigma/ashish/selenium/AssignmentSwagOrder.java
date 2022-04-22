f0561dde91932c374d6852a01cabedcafa722e91
Ashishthopate <ashishthopte27@gmail.com>
2022-04-22 08:29:42 +0530

    Added readProp Prog.

--- src/main/java/org/matrix/sigma/ashish/selenium/AssignmentSwagOrder.java
+++ src/main/java/org/matrix/sigma/ashish/selenium/AssignmentSwagOrder.java
@@ -5,15 +5,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class AssignmentSwagOrder {
 
-    public static void main(String[] args) {
+    public static String  readProp(String key) throws IOException {
+
+        FileInputStream fin=new FileInputStream("C:\\Users\\Ashish\\auto\\corejava-selenium-march22\\src\\main\\java\\org\\matrix\\sigma\\ashish\\selenium\\config.properties");
+
+        Properties prop=new Properties();
+        prop.load(fin);
+
+        String value=prop.getProperty(key);
+        return value;
+    }
+
+    public static void main(String[] args) throws IOException {
 
         System.setProperty("webdriver.gecko.driver", "D:\\geckodriver-v0.30.0-win32\\geckodriver.exe");
 
         WebDriver driver= new FirefoxDriver();
 
-        driver.get("https://www.saucedemo.com/");
+        driver.get(readProp("url"));
 
         driver.manage().window().maximize();
 
@@ -21,9 +36,10 @@ public class AssignmentSwagOrder {
 
        // String pass=driver.findElement(By.xpath("//div[text()='secret_sauce']")).getText();
 
-          driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("user-name")).sendKeys(readProp("user-name"));
 
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("password")).sendKeys(readProp("password"));
+
 
         driver.findElement(By.id("login-button")).click();
 
