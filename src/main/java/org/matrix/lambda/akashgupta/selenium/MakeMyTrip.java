611aa41452d6d7054d3572e6f7323652e9d269ac
AkashNGupta <akashngupta2116@gmail.com>
2022-04-23 18:15:02 +0530

    Assignment 12 translate(text(),'abc','ABC') convert text of element from lower to upper

--- src/main/java/org/matrix/lambda/akashgupta/selenium/MakeMyTrip.java
+++ src/main/java/org/matrix/lambda/akashgupta/selenium/MakeMyTrip.java
@@ -2,14 +2,12 @@ package org.matrix.lambda.akashgupta.selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import java.util.List;
-import java.util.Locale;
 
 public class MakeMyTrip {
 
@@ -18,24 +16,14 @@ public class MakeMyTrip {
     public void getLinks(String header) throws InterruptedException {
 
         driver.get("https://www.makemytrip.com/");
-        Dimension size = new Dimension(20, 20);
-        driver.manage().window().setSize(size);
-
-        String[] keywords = header.split(" ");
-        keywords[0] = keywords[0].toLowerCase(Locale.ROOT);
-        keywords[0] = keywords[0].replace(keywords[0].charAt(0), Character.toUpperCase(keywords[0].charAt(0)));
-        String xpath = "//*[contains(text(),'" + keywords[0] + "')]/following-sibling::ul[1]/child::li";
-        System.out.println(xpath);
-        /*WebElement single = driver.findElement(By.xpath("//a[text()='Flights']"));
-        Thread.sleep(2000);
-        System.out.println(single.getText());*/
+        String xpath = "//p[translate(text(),'abcdefghijklmnopqrstuvwxzy','ABCDEFGHIJKLMNOPQRSTUVWXZY')='" + header + "']/following-sibling::ul[1]/li/a";
+        //System.out.println(xpath);
         List<WebElement> links = driver.findElements(By.xpath(xpath));
         System.out.println("Total links present under " + header + " are :" + links.size());
-        for (int i = 0; i < links.size(); i++) {
-            //Thread.sleep(1000);
-            String text = links.get(i).getAttribute("data-cy").replaceAll("\\d", "");
-            System.out.println("Link" + (i + 1) + " : " + text.replaceAll("-", ""));
-            //System.out.println("links"+(i+1)+":"+links.get(i).getText());
+        int i = 1;
+        for (WebElement e : links) {
+            System.out.println("link " + i + ":" + e.getText());
+            i++;
         }
     }
 
@@ -43,8 +31,7 @@ public class MakeMyTrip {
     void makeMyTrip() throws InterruptedException {
         WebDriverManager.edgedriver().setup();
         driver = new ChromeDriver();
-        //please separate words before passing in getLink for MakeMyTrip header
-        getLinks("ABOUT THE SITE");
+        getLinks("PRODUCT OFFERING");
         driver.close();
     }
 }
