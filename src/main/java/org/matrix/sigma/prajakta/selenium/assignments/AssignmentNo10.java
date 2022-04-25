3ab0614fbd263cbbeb6f859b36082ed566dbbcec
deshpandeprajakta <deshpande.pa02@gmail.com>
2022-04-25 19:17:26 +0530

    Assignment 10 : Verify UI components

--- src/main/java/org/matrix/sigma/prajakta/selenium/assignments/AssignmentNo10.java
+++ src/main/java/org/matrix/sigma/prajakta/selenium/assignments/AssignmentNo10.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
@@ -15,17 +16,17 @@ import java.util.Properties;
 public class AssignmentNo10 {
 
     public static String readProperty(String key) throws IOException {
-        FileInputStream fin=new FileInputStream("D:\\Automation\\corejava-selenium-march22\\src\\main\\java\\org\\matrix\\sigma\\prajakta\\selenium\\assignments\\read.properties");
-        Properties prop=new Properties();
+        FileInputStream fin = new FileInputStream("D:\\Automation\\corejava-selenium-march22\\src\\main\\java\\org\\matrix\\sigma\\prajakta\\selenium\\assignments\\read.properties");
+        Properties prop = new Properties();
         prop.load(fin);
-        String value=prop.getProperty(key);
+        String value = prop.getProperty(key);
         return value;
     }
 
     @Test
     public void verify() throws IOException, InterruptedException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get(readProperty("hospital_url"));
         driver.manage().window().maximize();
         Thread.sleep(2000);
@@ -43,38 +44,33 @@ public class AssignmentNo10 {
         Thread.sleep(2000);
 
         // To check list elements are displayed or not
-        WebElement ele=driver.findElement(By.xpath("//ul[@class='main-navigation-menu']//li"));
+        WebElement ele = driver.findElement(By.xpath("//ul[@class='main-navigation-menu']//li"));
 
-        boolean status= ele.isDisplayed();
-        if(status){
-            System.out.println("Elements are displayed");
-        }else {
-            System.out.println("Test case fail");
-        }
+        boolean status = ele.isDisplayed();
+        Assert.assertTrue(status);
 
         // To display all elements
         System.out.println("---------------------------------------------------------");
-        List<WebElement> list=driver.findElements(By.xpath("//ul[@class='main-navigation-menu']//li"));
-        for (WebElement abc: list) {
-            System.out.println(" UL list Elements ::"+abc.getText());
+        List<WebElement> list = driver.findElements(By.xpath("//ul[@class='main-navigation-menu']//li"));
+        System.out.println("---UL list elements---");
+        for (WebElement abc : list) {
+            System.out.println(abc.getText());
         }
 
         //To check elements are displayed or not
-        WebElement ele2=driver.findElement(By.xpath("//div[@class='container-fluid container-fullw bg-white']//div[@class='col-sm-4']"));
+        WebElement ele2 = driver.findElement(By.xpath("//div[@class='container-fluid container-fullw bg-white']//div[@class='col-sm-4']"));
 
-        boolean check=ele2.isDisplayed();
-        if(check){
-            System.out.println("All elements are displayed");
-        }else {
-            System.out.println("Test case fail");
-        }
+        boolean check = ele2.isDisplayed();
+        Assert.assertTrue(check);
 
         //To print all elements
         System.out.println("------------------------------------------------------------");
-        List<WebElement> list1=driver.findElements(By.xpath("//div[@class='container-fluid container-fullw bg-white']//div[@class='col-sm-4']"));
-        for (WebElement web2: list1) {
-            System.out.println("Container Elements  :: "+web2.getText());
+        List<WebElement> list1 = driver.findElements(By.xpath("//div[@class='container-fluid container-fullw bg-white']//div[@class='col-sm-4']"));
+        System.out.println("---Container elements----");
+        for (WebElement web2 : list1) {
+            System.out.println(web2.getText());
         }
-      driver.close();
+
+        driver.close();
     }
 }
