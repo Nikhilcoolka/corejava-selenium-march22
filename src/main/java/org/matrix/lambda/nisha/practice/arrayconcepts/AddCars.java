817b79bd9a561eda89484d7c1afffc28951dee12
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-26 22:54:46 +0530

    MAT-13-NishaV: List of cars

--- src/main/java/org/matrix/lambda/nisha/practice/arrayconcepts/AddCars.java
+++ src/main/java/org/matrix/lambda/nisha/practice/arrayconcepts/AddCars.java
@@ -5,7 +5,15 @@ import java.util.ArrayList;
 public class AddCars {
 
     public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("Honda");
+        cars.add("BMW");
+        cars.add("Rolls Royce");
+        cars.add("Lamborghini");
+        cars.add("Audi");
 
+        System.out.println(cars);
 
     }
 }
