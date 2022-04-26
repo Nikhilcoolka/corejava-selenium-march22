73d7fe9fcd8526e2b86306485505c8d34a4edff8
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-26 22:46:29 +0530

    MAT-13-NishaV: Catch Test_Multiple_Catch_Block_2

--- src/main/java/org/matrix/lambda/nisha/practice/javatrycatch/Test_Multiple_Catch_Block_2.java
+++ src/main/java/org/matrix/lambda/nisha/practice/javatrycatch/Test_Multiple_Catch_Block_2.java
@@ -8,3 +8,11 @@ public class Test_Multiple_Catch_Block_2 {
             int arr [] = new int[5];
             arr[5] = 48/0;
         }
+
+        catch (Exception abc) {System.out.println("Common task completed");}
+        //catch (ArithmeticException ref) {System.out.println("Task 1 is completed");}
+        //catch (ArrayIndexOutOfBoundsException xyz) {System.out.println("Task 2 is completed");}
+
+        System.out.println("rest of the code......");
+    }
+}
