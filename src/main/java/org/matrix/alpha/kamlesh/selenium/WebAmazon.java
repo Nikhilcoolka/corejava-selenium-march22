21d06abd6d9a52fd08dc101e3200d892955b4ab8
Kamlesh Dandi <dandi8626@gmail.com>
2022-04-26 08:57:01 +0530

    Practice

--- src/main/java/org/matrix/alpha/kamlesh/selenium/WebAmazon.java
+++ src/main/java/org/matrix/alpha/kamlesh/selenium/WebAmazon.java
@@ -1,21 +1,26 @@
 package org.matrix.alpha.kamlesh.selenium;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.xml.crypto.dsig.spec.DigestMethodParameterSpec;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
-public class WebAmazon {
+public class
+WebAmazon {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.in/");
         driver.findElement(By.xpath("//span[@id='nav-link-accountList-nav-line-1']")).click();
-        Dimension size= new Dimension(1080,1080);
-        driver.manage().window().setSize(size);
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
+       // Dimension size= new Dimension(1080,1080);
+        //driver.manage().window().setSize(size);
+        driver.manage().window().maximize();
         driver.findElement(By.id("ap_email")).sendKeys("padmajatradingcompany@gmail.com");
         driver.findElement(By.id("continue")).click();
         driver.findElement(By.id("ap_password")).sendKeys("9372636305Rr@#");
@@ -27,11 +32,32 @@ public class WebAmazon {
         driver.findElement(By.id("adm-load-amount")).sendKeys(Keys.TAB);
         driver.findElement(By.xpath("//input[@value='Checkout']")).click();
         driver.findElement(By.xpath("//input[@value='Continue']")).click();
-        driver.findElement(By.xpath("//input[@id='pp-ey6wDU-187']")).click();
-        //driver.findElement(By.id("pp-ey6wDU-197")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("addCreditCardVerificationNumber4")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@name='addCreditCardVerificationNumber4']")).sendKeys("123");
+
+
+
+
+       /* driver.findElement(By.id("pp-FN6Qro-164")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@class = 'a-input-text a-form-normal a-width-small a-form-error'")).click();
+       */
+
+        /* driver.findElement(By.xpath("//input[@id='pp-ey6wDU-187']")).click();
+        driver.findElement(By.id("pp-qnnyeO-187")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@name='addCreditCardVerificationNumber4']")).sendKeys("100");
+        Thread.sleep(2000);*/
+        /*driver.findElement(By.xpath("//input[@id='pp-ey6wDU-187']")).sendKeys(Keys.TAB);
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//input[@id='pp-ey6wDU-187']")).sendKeys(Keys.TAB);
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//input[@id='pp-ey6wDU-187']")).sendKeys(Keys.TAB);
-        driver.findElement(By.xpath("//input[@id='pp-ey6wDU-187']")).sendKeys(Keys.TAB);
+        Thread.sleep(2000);
         driver.findElement(By.id("//input[@id='pp-ey6wDU-197']")).click();
+         */
     }
+
 }
