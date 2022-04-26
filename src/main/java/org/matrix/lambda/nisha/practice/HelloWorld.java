5ee2df39ed13c6fc3c12f005279a6ab64c4e3077
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-26 22:56:52 +0530

    MAT-13-NishaV: keyword static and void

--- src/main/java/org/matrix/lambda/nisha/practice/HelloWorld.java
+++ src/main/java/org/matrix/lambda/nisha/practice/HelloWorld.java
@@ -17,5 +17,14 @@ public class HelloWorld {
 
 # public = This keyword is an access modifier that represents visibility. It means it is visible to all.
 
+# static =  It is a keyword. If we declare any method as static, it is known as the static method.
+            The core advantage of the static method is that there is no need to create an object to
+            invoke the static method. The main() method is executed by the JVM, so it doesn't require
+            creating an object to invoke the main() method. So, it saves memory.
+
+# void = It is the return type of the method. It means it doesn't return any value.
+
+
+*/
 
 
