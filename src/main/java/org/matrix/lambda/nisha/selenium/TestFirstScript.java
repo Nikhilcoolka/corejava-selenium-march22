0ae183683d64526aec366c439aba150b8ac9add8
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-04-30 20:25:29 +0530

    MAT-13-NishaV: New Commits

--- src/main/java/org/matrix/lambda/nisha/selenium/TestFirstScript.java
+++ src/main/java/org/matrix/lambda/nisha/selenium/TestFirstScript.java
@@ -4,14 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestFirstScript {
 
     public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver","D:\\Ni\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\Ni\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();           // Driver Drives the chrome page for Intellij
+        // WebDriver is used for all web browsers like chrome, firefox, internet explorer
+
+        // WebDriver driver = new FirefoxDriver();
+        // WebDriver driver = new EdgeDriver();
+
         driver.get("https://demo.nopcommerce.com/");        // Chrome website
 
         driver.manage().window().maximize();                // Maximize the window
@@ -28,18 +35,21 @@ public class TestFirstScript {
         Thread.sleep(2000);
         driver.findElement(By.xpath("//button[text()='Log in']")).click();                           // Click on login
 
-        WebElement errorMessage = driver.findElement(By.xpath("//div[text()='Login was unsuccessful. Please correct the errors and try again.'"));
-         boolean status = errorMessage.isDisplayed();
+        //WebElement errorMessage = driver.findElement(By.xpath("//div[contains(text(),'Login was unsuccessful. Please correct the errors and try again.')]"));
+        WebElement errorMessage = driver.findElement(By.xpath("//div[text()='Login was unsuccessful. Please correct the errors and try again.']"));
+        boolean status = errorMessage.isDisplayed();
 
-         if (status)
-         {
-             System.out.println("Test case passed.");
-         }else
-         {
-             System.out.println("Test case failed.");
-         }
+        if (status) {
+            System.out.println("Test case passed.");
+        } else {
+            System.out.println("Test case failed.");
+        }
 
-         Thread.sleep(2000);
-         driver.close();
+        Thread.sleep(2000);
+        driver.close();
+
     }
 }
+
+
+
