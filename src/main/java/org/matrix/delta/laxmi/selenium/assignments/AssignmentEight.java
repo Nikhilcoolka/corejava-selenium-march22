9bb464d1c977da395610f970245f56fd13ee5808
laxmikurhade <laxmikurhade09@gmail.com>
2022-05-04 22:20:47 +0530

    AssignmentEight.java using testng added

--- src/main/java/org/matrix/delta/laxmi/selenium/assignments/AssignmentEight.java
+++ src/main/java/org/matrix/delta/laxmi/selenium/assignments/AssignmentEight.java
@@ -3,12 +3,19 @@ package org.matrix.delta.laxmi.selenium.assignments;
 import org.matrix.delta.laxmi.selenium.TestPropertiesScript;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AssignmentEight {
-    public static void main(String[] args) throws IOException, InterruptedException {
+
+    @Test
+    public static void verifyFirstMainMenuLinks() throws IOException, InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get(TestPropertiesScript.readPropertyDemo("orange_hrm_url"));
 
@@ -19,14 +26,26 @@ public class AssignmentEight {
 
         driver.findElement(By.id("btnLogin")).click();
         Thread.sleep(2000);
-        /*String pageTitle= driver.getTitle();
 
-        if(pageTitle.equals("Swag Labs")){
-            System.out.println("Test is Pass");
-            System.out.println("Login Successful");
+        ArrayList<String> expectedLinks = new ArrayList<>();
+        expectedLinks.add("Admin");
+        expectedLinks.add("PIM");
+        expectedLinks.add("Leave");
+        expectedLinks.add("Time");
+        expectedLinks.add("Recruitment");
+        expectedLinks.add("My Info");
+        expectedLinks.add("Performance");
+        expectedLinks.add("Dashboard");
+        expectedLinks.add("Directory");
+        expectedLinks.add("Maintenance");
+        expectedLinks.add("Buzz");
+
+        ArrayList<String> actualLinks = new ArrayList<>();
+        List<WebElement> links = driver.findElements(By.xpath("//a[@class='firstLevelMenu']//b"));
+        for (WebElement element : links) {
+            actualLinks.add(element.getText());
         }
-        else{
-            System.out.println("Test and login is Fail");
-        }*/
+        Assert.assertEquals(actualLinks, expectedLinks);
+        driver.close();
     }
 }
